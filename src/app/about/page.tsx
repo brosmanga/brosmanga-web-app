a4ef4eaa95dfb228a55aae50de37d44b58ec704a
Fabiano <fabianorocha102003@gmail.com>
2024-11-30 20:46:10 -0300

    Remove unused import

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import AboutContent from "@/components/AboutContent";
-import { useRouter } from "next/navigation";
 
 const AboutPage: React.FC = () => {
   return (
